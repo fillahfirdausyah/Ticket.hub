07a1ab77cef8ac271347d25cd2e4bd512d3c9f12
firdausyah22 <39095849+firdausyah22@users.noreply.github.com>
2019-12-08 12:55:09 +0700

    Menambahkan halaman admin

--- main.cpp
+++ main.cpp
@@ -6,31 +6,53 @@
 
 using namespace std;
 
-//deklarasi function
+
 void judul() {
 	cout << "\t\t\t\t          ##############################" << endl;
 	cout << "\t\t\t\t          #                            #" << endl;
 	cout << "\t\t\t\t          #          Ticket.hub        #" << endl;
-	cout << "\t\t\t\t          #                            #" << endl;
+	cout << "\t\t\t\t          #             V2.4           #" << endl;
 	cout << "\t\t\t\t          ##############################" << endl;
 }
+
+void bersih() {
+	system("cls");
+}
+
+//ProtoType Function
+void menuUtama();
 void login();
-void daftar();
 void lupa();
 void help();
 void about();
+void mainAdmin();
 
+//Main Program
 main () {
 	judul();
+	menuUtama();
 
+getch();
+}
+
+
+
+// Proses Program
+// Function
+
+//Tampilkan Menu
+void menuUtama() {
 	int pilih;
+	cout << "\tMenu Utama" << endl;
+	cout << "\t====================================" << endl;
 	cout << "\t1.Login" << endl;
-	cout << "\t2.Daftar" << endl;
-	cout << "\t3.Lupa" << endl;
-	cout << "\t4.Help" << endl;
-	cout << "\t5.About" << endl;
+	cout << "\t2.Lupa" << endl;
+	cout << "\t3.Help" << endl;
+	cout << "\t4.About" << endl;
+	cout << "\t====================================" << endl;
 	cout << "\tPilih Menu : ";
 	cin >> pilih;
+
 
 	switch (pilih) {
  		case 1:
@@ -38,40 +60,39 @@ main () {
  		break;
 
  		case 2:
- 			daftar();
- 		break;
-
- 		case 3:
  			lupa();
  		break;
 
- 		case 4:
+ 		case 3:
  			help();
  		break;
 
- 		case 5:
+ 		case 4:
  			about();
  		break;
-	};
 
-getch();
+	};
 }
 
+//Proses Login
 void login() {
+
+	bersih();
+	judul();
+
 	string user;
 	string pass;
-
 	cout << "\tUsername : ";
 	cin >> user;
 	cout << "\tPassword : ";
 	cin >> pass;
 
 		if (user == "admin" && pass == "admin123") {
-			system("cls");
+			bersih();
 			judul();
-			cout << "\tWelcome " << user << endl;
-		}else if (user == "user" && pass == "user123") {
-			system("cls");
+			mainAdmin();
+		}else if (user == "kasir" && pass == "kasir123") {
+			bersih();
 			judul();
 			cout << "\tWelcome " << user << endl;
 		}else {
@@ -79,18 +100,53 @@ void login() {
 		}
 }
 
-void daftar() {
-
-}
-
+//Jika User memilih Lupa
 void lupa() {
 
 }
 
+//Jika User memilih Help
 void help() {
+	bersih ();
+	judul  ();
+
+	cout << "\t\t\t\t   Selamat Datang di pusat bantuan Tickte.hub";
 
 }
 
+//Jika User memilih About
 void about() {
 
+}
+
+void mainAdmin() {
+	bersih();
+	judul();
+
+	int pAdmin;
+	cout << "\tSelamat Datang admin"  << endl;
+	cout << "\t====================================" << endl;
+	cout << "\t1. Tambah Data Ticket" << endl;
+	cout << "\t2. Lihat Data Ticket"  << endl;
+	cout << "\t3. Edit Data Ticket"   << endl;
+	cout << "\t4. Hapus Data Ticket"  << endl;
+	cout << "\t====================================" << endl;
+	cout << "\tPilih Menu : ";
+	cin >> pAdmin;
+
+	enum option {CREATE = 1, READ, UPDATE, DELETE};
+	switch (pAdmin) {
+		case CREATE:
+			cout << "\tTambah Data Ticket : ";
+			break;
+		case READ:
+			cout << "\tLihat Data Ticket : ";
+			break;
+		case UPDATE:
+			cout << "\tEdit Data Ticket : ";
+			break;
+		case DELETE:
+			cout << "\tHapus Data Ticket : ";
+			break;
+	}
 }
